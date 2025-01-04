d4af3030beae499764ef678d4797036cf3dbbb41
kajalyadav <kajall1402@gmail.com>
2025-01-04 10:43:56 +0530

    Add files via upload

---
+++ javascript/datatypes.js
@@ -0,0 +1,58 @@
+// var x1=12
+// var x2=12
+// let x3=23
+// console.log(x1==x2)//true
+// console.log(x3===x2)//false
+
+
+//non-primitive
+// obj={a:11,b:12}
+// arr=[1,2,3,4]
+
+
+// var emp={id:10,name:'kajal',salary:100000}
+// document.write(emp.id+" , "+emp.name+" , "+emp.salary)
+
+
+// var a1=['t1','t2','t3']
+// var l=a1.length
+// console.log(l)//3
+// console.log(a1[1])//t2
+// console.log(a1[l-1])//t3
+
+
+// var a3=['a','b','c','d','e','f']
+// for (let index = 0; index < a3.length; index++) {
+//     console.log(a3[index]+'\n');
+// }
+
+
+// let a4=[3,4,5,6,7]
+// let a5=[]
+// for (let index = 0; index < a4.length; index++) {
+//     a5[index]=a4[index]*3
+// }
+// console.log(a5);
+
+
+var a=undefined
+var b='test'
+var c=true
+var d=5
+var e=null
+var f=new Number(5)
+document.write(typeof(a)+'<br>')//undefined
+document.write(typeof(b)+'<br>')//string
+document.write(typeof(c)+'<br>')//boolean
+document.write(typeof(d)+'<br>')//number
+document.write(typeof(e)+'<br>')//object
+document.write(typeof(f)+'<br>')//object
+
+
+
+
+
+
+
+
+
